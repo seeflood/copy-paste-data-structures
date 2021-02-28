e89a92939dd0042f1707c3dac4d64667d4ac1cd2
seeflood <349895584@qq.com>
2021-02-28 21:34:41 +0800

    add unionfind

---
+++ src/main/go/io/github/seeflood/copy-paste-ds/UnionFind.go
@@ -0,0 +1,70 @@
+package copy_paste_ds
+
+type UnionFind struct {
+	n          int
+	components int
+	parent     []int
+	size       []int
+}
+
+func NewUnionFind(n int) *UnionFind {
+	parent := make([]int, n)
+	size := make([]int, n)
+	for i := 0; i < n; i++ {
+		parent[i] = i
+		size[i] = 1
+	}
+
+	return &UnionFind{
+		n:          n,
+		components: n,
+		parent:     parent,
+		size:       size,
+	}
+}
+
+func (uf *UnionFind) Union(a int, b int) {
+	if a < 0 || a >= uf.n || b < 0 || b >= uf.n {
+		return
+	}
+	aRoot := uf.FindRoot(a)
+	bRoot := uf.FindRoot(b)
+	if aRoot == bRoot {
+		return
+	}
+	if uf.size[aRoot] < uf.size[bRoot] {
+		uf.parent[aRoot] = bRoot
+		uf.size[bRoot] += uf.size[aRoot]
+	} else {
+		uf.parent[bRoot] = aRoot
+		uf.size[aRoot] += uf.size[bRoot]
+	}
+	uf.components--
+}
+
+func (uf *UnionFind) Connected(a int, b int) bool {
+	if a < 0 || a >= uf.n || b < 0 || b >= uf.n {
+		return false
+	}
+	return uf.FindRoot(a) == uf.FindRoot(b)
+}
+
+func (uf *UnionFind) FindRoot(a int) int {
+	if a < 0 || a >= uf.n {
+		return -1
+	}
+	root := a
+	for uf.parent[root] != root {
+		root = uf.parent[root]
+	}
+	for a != root {
+		newA := uf.parent[a]
+		uf.parent[a] = root
+		a = newA
+	}
+	return root
+}
+
+func (uf *UnionFind) CountComponents() int {
+	return uf.components
+}
---
+++ src/main/go/io/github/seeflood/copy-paste-ds/UnionFind_test.go
@@ -0,0 +1,34 @@
+package copy_paste_ds
+
+import (
+	"testing"
+)
+
+func TestUnionFind(t *testing.T) {
+	uf := NewUnionFind(10)
+	AssertTrue(uf.CountComponents() == 10, t)
+	AssertTrue(!uf.Connected(1, 2), t)
+	AssertTrue(!uf.Connected(3, 4), t)
+	AssertTrue(!uf.Connected(3, 4), t)
+	uf.Union(1, 2)
+	AssertTrue(uf.Connected(1, 2), t)
+	AssertTrue(uf.CountComponents() == 9, t)
+	uf.Union(1, 4)
+	AssertTrue(uf.Connected(1, 2), t)
+	AssertTrue(uf.Connected(1, 4), t)
+	AssertTrue(uf.Connected(2, 4), t)
+	AssertTrue(uf.CountComponents() == 8, t)
+	uf.Union(2, 3)
+	AssertTrue(uf.Connected(1, 3), t)
+	AssertTrue(uf.Connected(1, 4), t)
+	AssertTrue(uf.Connected(2, 4), t)
+	AssertTrue(uf.Connected(3, 4), t)
+	AssertTrue(uf.CountComponents() == 7, t)
+
+}
+
+func AssertTrue(b bool, t *testing.T) {
+	if !b {
+		t.Error()
+	}
+}
