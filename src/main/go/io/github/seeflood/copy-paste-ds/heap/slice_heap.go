347d31686adfd301a0ba32cf66f709908ea6ef69
seeflood <349895584@qq.com>
2021-03-07 17:31:29 +0800

    add slice_heap for go

---
+++ src/main/go/io/github/seeflood/copy-paste-ds/heap/slice_heap.go
@@ -0,0 +1,39 @@
+package heap
+
+import "container/heap"
+
+type Comparator interface {
+	compare(a interface{}, b interface{}) int
+}
+type SliceHeap struct {
+	slice      []interface{}
+	comparator Comparator
+}
+
+func NewSliceHeap(c Comparator) *SliceHeap {
+	h := &SliceHeap{
+		make([]interface{}, 0),
+		c,
+	}
+	heap.Init(h)
+	return h
+}
+
+func (h *SliceHeap) IsEmpty() bool {
+	return h.Len() == 0
+}
+func (h *SliceHeap) Len() int           { return len(h.slice) }
+func (h *SliceHeap) Less(i, j int) bool { return h.comparator.compare(h.slice[i], h.slice[j]) < 0 }
+func (h *SliceHeap) Swap(i, j int)      { h.slice[i], h.slice[j] = h.slice[j], h.slice[i] }
+
+func (h *SliceHeap) Push(x interface{}) {
+	h.slice = append(h.slice, x)
+}
+
+func (h *SliceHeap) Pop() interface{} {
+	old := h.slice
+	n := len(old)
+	x := old[n-1]
+	h.slice = old[0 : n-1]
+	return x
+}
---
+++ src/main/go/io/github/seeflood/copy-paste-ds/heap/slice_heap_test.go
@@ -0,0 +1,38 @@
+package heap
+
+import (
+	"container/heap"
+	"testing"
+)
+
+type IntComparator struct {
+}
+
+func (*IntComparator) compare(a interface{}, b interface{}) int {
+	return a.(int) - b.(int)
+}
+
+func TestSliceHeap(t *testing.T) {
+	h := NewSliceHeap(&IntComparator{})
+	AssertTrue(h.IsEmpty(), t)
+	heap.Push(h, 3)
+	heap.Push(h, 5)
+	heap.Push(h, 1)
+	heap.Push(h, 8)
+	heap.Push(h, 2)
+
+	AssertTrue(heap.Pop(h) == 1, t)
+	AssertTrue(heap.Pop(h) == 2, t)
+	AssertTrue(heap.Pop(h) == 3, t)
+	AssertTrue(heap.Pop(h) == 5, t)
+	AssertTrue(!h.IsEmpty(), t)
+	AssertTrue(heap.Pop(h) == 8, t)
+	AssertTrue(h.Len() == 0, t)
+	AssertTrue(h.IsEmpty(), t)
+}
+
+func AssertTrue(b bool, t *testing.T) {
+	if !b {
+		t.Error()
+	}
+}
