c5c95c9180e9e80dc892bbf75886ea975bf8b29c
seeflood <349895584@qq.com>
2021-03-14 13:40:13 +0800

    optimize slice_heap

--- src/main/go/io/github/seeflood/copy-paste-ds/heap/slice_heap_test.go
+++ src/main/go/io/github/seeflood/copy-paste-ds/heap/slice_heap_test.go
@@ -1,16 +1,9 @@
-package heap
+package slice_heap
 
 import (
 	"container/heap"
 	"testing"
 )
-
-type IntComparator struct {
-}
-
-func (*IntComparator) compare(a interface{}, b interface{}) int {
-	return a.(int) - b.(int)
-}
 
 func TestSliceHeap(t *testing.T) {
 	h := NewSliceHeap(&IntComparator{})
@@ -21,9 +14,36 @@ func TestSliceHeap(t *testing.T) {
 	heap.Push(h, 8)
 	heap.Push(h, 2)
 
+	AssertTrue(Peek(h) == 1, t)
 	AssertTrue(heap.Pop(h) == 1, t)
+	AssertTrue(Peek(h) == 2, t)
 	AssertTrue(heap.Pop(h) == 2, t)
+	AssertTrue(Peek(h) == 3, t)
 	AssertTrue(heap.Pop(h) == 3, t)
+	AssertTrue(Peek(h) == 5, t)
+	AssertTrue(heap.Pop(h) == 5, t)
+	AssertTrue(!h.IsEmpty(), t)
+	AssertTrue(heap.Pop(h) == 8, t)
+	AssertTrue(h.Len() == 0, t)
+	AssertTrue(h.IsEmpty(), t)
+}
+
+func TestIntSliceHeap(t *testing.T) {
+	h := NewIntSliceHeap()
+	AssertTrue(h.IsEmpty(), t)
+	heap.Push(h, 3)
+	heap.Push(h, 5)
+	heap.Push(h, 1)
+	heap.Push(h, 8)
+	heap.Push(h, 2)
+
+	AssertTrue(Peek(h) == 1, t)
+	AssertTrue(heap.Pop(h) == 1, t)
+	AssertTrue(Peek(h) == 2, t)
+	AssertTrue(heap.Pop(h) == 2, t)
+	AssertTrue(Peek(h) == 3, t)
+	AssertTrue(heap.Pop(h) == 3, t)
+	AssertTrue(Peek(h) == 5, t)
 	AssertTrue(heap.Pop(h) == 5, t)
 	AssertTrue(!h.IsEmpty(), t)
 	AssertTrue(heap.Pop(h) == 8, t)
@@ -32,6 +52,7 @@ func TestSliceHeap(t *testing.T) {
 }
 
 func AssertTrue(b bool, t *testing.T) {
+	t.Helper()
 	if !b {
 		t.Error()
 	}
